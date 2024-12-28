003de97637f80ddd9ffcd3d307e9bddb5d0827d9
Jaafar Fora <jaafar.fora@mimacom.com>
2024-12-28 11:54:51 +0100

    add login script to implement account locking

--- Project1-register-login/scripts/login.js
+++ Project1-register-login/scripts/login.js
@@ -1,11 +1,21 @@
 import { validateFormGroup } from '../scripts/validation.js';
-import { existUserByEmail, isCorrectPassword } from './userDB.js';
+import {
+  existUserByEmail,
+  isCorrectPassword,
+  incrementLoginCounter,
+  lockUserAccount,
+  isUserAccountLocked,
+  resetLoginCounter,
+} from './userDB.js';
 
 // Global errors div
 const gloablerror = document.querySelector('#gloablerror');
 
 // Notification errors div
 const notification = document.querySelector('#notification');
+
+// Login Button
+const loginButton = document.querySelector('#loginButton');
 
 // Form element
 const loginForm = document.querySelector('#loginForm');
@@ -28,27 +38,49 @@ passwordInput.addEventListener('blur', (event) => {
 
 function onLoginButtonClick(event) {
   event.preventDefault();
+  gloablerror.textContent = '';
   const isEmailValid = validateFormGroup(emailFormGroup);
   const isPasswordValid = validateFormGroup(passwordFormGroup);
 
   if (isEmailValid && isPasswordValid) {
     const user = {
-      "email": emailInput.value,
-      "password": passwordInput.value,
+      email: emailInput.value,
+      password: passwordInput.value,
     };
-    loginUser(user);
+    if (!existUserByEmail(user)) {
+      gloablerror.textContent = 'Email or password incorrect';
+    } else {
+      loginUser(user);
+    }
   }
 }
 
 function loginUser(user) {
-  gloablerror.textContent = ''
-  if (existUserByEmail(user) && isCorrectPassword(user)) {
-    notification.textContent = 'Login successful !'
-    setTimeout(() => {
-      notification.textContent = ''
-      // redirect
-    }, 5000)
+  gloablerror.textContent = '';
+  if (isUserAccountLocked(user)) {
+    gloablerror.textContent =
+      'Your account is locked out, try again later';
   } else {
-    gloablerror.textContent = 'Email or password incorrect'
+    if (isCorrectPassword(user)) {
+      successfulLogin();
+    } else {
+      gloablerror.textContent = 'Email or password incorrect';
+      const loginCounter = incrementLoginCounter(user);
+      if (loginCounter == 3) {
+        gloablerror.textContent =
+      'Your account is locked out, try again in 1 minute';
+        resetLoginCounter(user);
+        lockUserAccount(user)
+      }
+    }
   }
+}
+
+function successfulLogin() {
+  notification.textContent = 'Login successful !';
+  loginButton.disabled = true;
+  setTimeout(() => {
+    notification.textContent = '';
+    window.location.href = 'users.html';
+  }, 5000);
 }
--- Project1-register-login/scripts/userDB.js
+++ Project1-register-login/scripts/userDB.js
@@ -1,7 +1,6 @@
 function getUsers() {
   const usersInStorage = localStorage.getItem('users');
-  const users = usersInStorage ? JSON.parse(usersInStorage) : [];
-  return users;
+  return usersInStorage ? JSON.parse(usersInStorage) : [];
 }
 
 function existUserByEmail(user) {
@@ -9,20 +8,53 @@ function existUserByEmail(user) {
   return users.some(existingUser => existingUser.email === user.email);
 }
 
-function isCorrectPassword(user) {
+function getUser(email) {
   const users = getUsers();
-  const storedUser = users.filter((su) => su.email === user.email)[0];
-  console.log(storedUser);
-  console.log(user);
-  
-  console.log(storedUser);
+  return users.find(user => user.email == email);
+}
+
+function isCorrectPassword(user) {
+  const storedUser = getUser(user.email);
   return storedUser.password == user.password;
 }
 
 function addUser(user) {
   const users = getUsers();
-  users.push(user);
+  users.push({...user, 'loginCounter': 0, lockingDate: null});
   localStorage.setItem('users', JSON.stringify(users));
 }
 
-export { existUserByEmail, addUser, isCorrectPassword }
+function isUserAccountLocked(user) {
+  const storedUser = getUser(user.email);
+  const lockingDate = new Date(storedUser.lockingDate);
+  const currentDate = new Date();
+  console.log(lockingDate && (lockingDate instanceof Date) && (lockingDate > currentDate));
+  return (lockingDate && (lockingDate instanceof Date) && (lockingDate > currentDate));
+}
+
+function lockUserAccount(user) {
+  const users = getUsers();
+  const storedUser = users.find((u) => u.email == user.email);
+  storedUser.lockingDate = addMinutes(new Date(), 1);
+  localStorage.setItem('users', JSON.stringify(users));
+}
+
+function resetLoginCounter(user) {
+  const users = getUsers();
+  const storedUser = users.find((u) => u.email == user.email);
+  storedUser.loginCounter = 0;
+  localStorage.setItem('users', JSON.stringify(users));
+}
+
+function incrementLoginCounter(user) {
+  const users = getUsers();
+  const storedUser = users.find((u) => u.email == user.email);
+  storedUser.loginCounter++;
+  localStorage.setItem('users', JSON.stringify(users));
+  return storedUser.loginCounter; 
+}
+
+function addMinutes(dt, minutes) {
+  return new Date(dt.getTime() + minutes*60000);
+}
+export { existUserByEmail, addUser, isCorrectPassword, incrementLoginCounter, resetLoginCounter, lockUserAccount, isUserAccountLocked }
